1d9ffefe8322c646aa11084d2cd358353122d975
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-20 09:02:04 -0500

    Reinventing the wheel. Again.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import About from "./components/About.svelte";
   export let name;
   export let lastName;
   let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
@@ -12,11 +13,6 @@
 
   :global(:root) {
     --theme-color: purple;
-  }
-
-  p {
-    color: var(--theme-color);
-    font-size: 22px;
   }
 
   main {
@@ -46,6 +42,6 @@
 
 <main>
   <h1>Hello {name} {lastName}!</h1>
-  <p>Frontend Developer</p>
+  <About />
   <img src={svelteLogo} alt="Svelte" />
 </main>
---
+++ src/components/About.svelte
@@ -0,0 +1,15 @@
+<script>
+ let someText = `Frontend Developer at Platzi`;
+</script>
+
+<style>
+  p {
+    color: var(--theme-color);
+  }
+</style>
+
+<div class="About">
+  <p>
+    {someText}
+  </p>
+</div>
