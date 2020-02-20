d9da3e15c55729464a8ca2b940661261ca5655c0
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-20 10:02:11 -0500

    Batman! (this commit has no parents)

--- src/App.svelte
+++ src/App.svelte
@@ -24,6 +24,11 @@
     --theme-color: purple;
   }
 
+  :global(body.dark-mode) {
+    background-color: #1d3040;
+    color: #bfc2c7;
+  }
+
   main {
     text-align: center;
     padding: 1em;
--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,15 @@
 <script>
   let someText = `Frontend Developer at Platzi`;
   let count = 0;
+  let styles = { darkMode: false };
 
   function handleClick() {
     count += 1;
+  }
+
+  function toggle() {
+    styles.darkMode = !styles.darkMode;
+    window.document.body.classList.toggle("dark-mode");
   }
 </script>
 
@@ -14,6 +20,15 @@
 </style>
 
 <div class="About">
-  <p>{someText}</p>
+
+  {#if !styles.darkMode}
+    <p>{someText}</p>
+  {:else}
+    <p>
+      <span>Hello DarkMode</span>
+    </p>
+  {/if}
+
   <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+  <button on:click={toggle}>DarkMode</button>
 </div>
