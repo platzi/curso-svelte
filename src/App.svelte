70e56fae24cd37ff228ed06ca065f5a3dd9bdaa1
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-20 08:48:33 -0500

    This is where it all begins...

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,33 @@
 <script>
   export let name;
   export let lastName;
+  let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
 </script>
 
 <style>
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+  }
+
+  :global(:root) {
+    --theme-color: purple;
+  }
+
+  p {
+    color: var(--theme-color);
+    font-size: 22px;
+  }
+
   main {
     text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+
+  main img {
+    width: 30%;
   }
 
   h1 {
@@ -27,9 +46,6 @@
 
 <main>
   <h1>Hello {name} {lastName}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <p>Frontend Developer</p>
+  <img src={svelteLogo} alt="Svelte" />
 </main>
